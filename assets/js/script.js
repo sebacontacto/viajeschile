59b5e33b2dd004ced29267841a4d0ef13e594bce
sebacontacto@gmail.com <sebacontacto@gmail.com>
2022-01-23 23:29:49 -0300

    Cuarto commit. Carrusel reparado a medias, aun no funciona en slide en load inicial

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,6 @@ $(document).ready(function() {
         interval: 4500
     });
 
-    $("a").click(function() {
-        var gato = this.hash
-
-        $("html, body").animate({
-                scrollTop: $(gato).offset().top - 70
-            },
-            800
-        )
-    });
 
 
 
