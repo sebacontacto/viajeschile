ae97bc4c80cfe88795dc22e82334c628974abae7
sebacontacto@gmail.com <sebacontacto@gmail.com>
2022-01-23 22:52:32 -0300

    Tercer commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,19 @@ $(document).ready(function() {
     /*Modificacion del intervalodel carrousel*/
     $('.carousel').carousel({
         interval: 4500
-    })
+    });
+
+    $("a").click(function() {
+        var gato = this.hash
+
+        $("html, body").animate({
+                scrollTop: $(gato).offset().top - 70
+            },
+            800
+        )
+    });
+
+
 
 });
 
@@ -10,4 +22,4 @@ $(document).ready(function() {
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
